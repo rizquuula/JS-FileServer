2391e6752652e805deef96f43f295598d512e654
M Razif Rizqullah <razifrizqullah@gmail.com>
2026-01-07 15:07:30 +0700

    set limit as config

--- app/config/index.js
+++ app/config/index.js
@@ -7,6 +7,7 @@ module.exports = {
   // Upload configuration
   UPLOAD_DIR: 'file',
   UPLOAD_PATH: path.join(__dirname, '..', 'file'),
+  FILE_SIZE_LIMIT: 10 * 1024 * 1024, // 10MB
 
   // Static files configuration
   STATIC_ROUTE: '/file',
--- app/middleware/upload.js
+++ app/middleware/upload.js
@@ -28,7 +28,7 @@ const upload = multer({
   storage,
   fileFilter,
   limits: {
-    fileSize: 10 * 1024 * 1024 // 10MB limit
+    fileSize: config.FILE_SIZE_LIMIT
   }
 });
 
